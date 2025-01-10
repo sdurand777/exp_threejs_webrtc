b4fb753a370d8355a719c955c7224ce1957726eb
ivm <stan777.durand777@gmail.com>
2025-01-10 15:02:22 +0100

    update repo

---
+++ public/js/simple.js
@@ -0,0 +1,36 @@
+import * as THREE from 'three';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
+import { PLYLoader } from 'three/addons/loaders/PLYLoader.js'
+
+// Créer la scène, la caméra et le rendu
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(new THREE.Color("lightblue"));
+document.body.appendChild(renderer.domElement);
+
+// Ajouter un cube à la scène
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// Positionner la caméra
+camera.position.z = 5;
+
+// Ajouter les OrbitControls
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// Fonction d'animation
+function animate() {
+  requestAnimationFrame(animate);
+
+  // Faire tourner le cube
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
---
+++ server.js
@@ -0,0 +1,113 @@
+/*
+ *
+ * This is the server which runs all Websocket and WebRTC communications for our application.
+ *
+ */
+
+// Set up an express application to run the server
+const express = require("express");
+const app = express();
+
+// tell our express application to serve the 'public' folder
+app.use(express.static("public"));
+
+// tell the server to listen on a given port
+const port = process.env.PORT || 8080;
+const server = app.listen(port);
+console.log("Webserver is running on http://localhost:" + port);
+
+// We will use the socket.io library to manage Websocket connections
+const io = require("socket.io")().listen(server);
+
+// add the database application 'nedb'
+const Datastore = require("nedb");
+// create our database and save it to a local file
+const db = new Datastore({ filename: "mydatabase.json", autoload: true });
+
+// We will use this object to store information about active peers
+let peers = {};
+
+function main() {
+  setupSocketServer();
+
+  // periodically update all peers with their positions
+  setInterval(function () {
+    io.sockets.emit("positions", peers);
+  }, 100);
+}
+
+main();
+
+function setupSocketServer() {
+  // Set up each socket connection
+  io.on("connection", (socket) => {
+    console.log(
+      "Peer joined with ID",
+      socket.id,
+      ". There are " + io.engine.clientsCount + " peer(s) connected."
+    );
+
+    // add a new peer indexed by their socket id
+    peers[socket.id] = {
+      position: [0, 0.5, 0],
+      rotation: [0, 0, 0, 1], // stored as XYZW values of Quaternion
+    };
+
+    // send the new peer a list of all other peers
+    socket.emit("introduction", Object.keys(peers));
+
+    // also give the peer all existing peers positions:
+    socket.emit("userPositions", peers);
+
+    // also give them existing data in the database
+    db.find({}, (err, docs) => {
+      if (err) return;
+      for (let i = 0; i < docs.length; i++) {
+        let doc = docs[i];
+        socket.emit("data", doc);
+      }
+    });
+
+    // tell everyone that a new user connected
+    io.emit("peerConnection", socket.id);
+
+    // whenever the peer moves, update their movements in the peers object
+    socket.on("move", (data) => {
+      if (peers[socket.id]) {
+        peers[socket.id].position = data[0];
+        peers[socket.id].rotation = data[1];
+      }
+    });
+
+    // setup a generic ping-pong which can be used to share arbitrary info between peers
+    socket.on("data", (data) => {
+      // insert data into the database
+      db.insert(data);
+
+      // then send it to all peers
+      io.sockets.emit("data", data);
+    });
+
+    // Relay simple-peer signals back and forth
+    socket.on("signal", (to, from, data) => {
+      if (to in peers) {
+        io.to(to).emit("signal", to, from, data);
+      } else {
+        console.log("Peer not found!");
+      }
+    });
+
+    // handle disconnections
+    socket.on("disconnect", () => {
+      delete peers[socket.id];
+      io.sockets.emit("peerDisconnection", socket.id);
+      console.log(
+        "Peer " +
+          socket.id +
+          " diconnected, there are " +
+          io.engine.clientsCount +
+          " peer(s) connected."
+      );
+    });
+  });
+}
